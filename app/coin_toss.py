07ed207f9a9d280079a67b522f2ddf415e7a0247
Damian Esene <damianesene@Damians-MacBook-Air.local>
2023-03-02 13:31:34 -0500

    test auto CD-1

--- app/coin_toss.py
+++ app/coin_toss.py
@@ -8,9 +8,9 @@ def home():
     ran_num = random.randint(0,1)
 
     if ran_num == 0:
-        return "Heads"
-    else:
         return "Tails"
+    else:
+        return "Heads"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
